6b634c2d011f756216d0ac45f189a24d109106bd
Sandor Lopez <sandor.lopez@elev.ga.ntig.se>
2024-09-11 15:04:21 +0200

    iuliulkuhbuilb

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Player 1";
+
+Console.WriteLine("vad representar tjuren inom ekonomiska termer");
+
+Console.WriteLine("a) När börsen rör sig uppåt under en längre tid b) När börsen som helhet, eller ett enskilt värdepapper, upplever en nedgång c) En strategi som ofta används för är att sälja av innehav när det blir skakigt");
+
+string tjur = Console.ReadLine();
+
+if (tjur.ToUpper() == "a" || score > 0);
+{
+    return 100;
+}
+else
+{
+    Console.WriteLine("Fel, nästa fråga")
+}
+
+int score = 100;
+score += 1;
+score ++;
+Console.WriteLine($"Du har {score}hp kvar");
+
+Console.ReadLine();
